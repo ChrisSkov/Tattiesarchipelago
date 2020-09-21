41bdbabb558b67463bc9118e902cfde1ed1aa7cd
Chris Skov <stofferdyr@gmail.com>
2020-09-21 21:56:50 +0200

    move stuff around. start script obj implementation

---
+++ TattieIsland/Assets/Scripts/ScriptObj/FloatVariable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Float", menuName = "ScriptableObjects/FloatVariable", order = 2)]
+public class FloatVariable : ScriptableObject
+{
+    public float Value;
+}
