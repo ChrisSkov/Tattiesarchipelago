66c5e16b2064a02d8d1024713a256676600bbbe4
Chris Skov <stofferdyr@gmail.com>
2020-09-25 10:35:33 +0200

    set up basic chicken stuff

---
+++ TattieIsland/Assets/DisplayChickenInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayChickenInfo : MonoBehaviour
+{
+    Slider hpBar;
+    Text nameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText = GetComponentInChildren<Text>();
+        hpBar = GetComponentInChildren<Slider>();
+    }
+
+    public void SetHPBarMaxValue(float maxHP)
+    {
+        hpBar.maxValue = maxHP;
+    }
+    public void DisplayChickenName(string name)
+    {
+        nameText.text = name;
+    }
+
+    public void UpdateHealthBar(float currentHp)
+    {
+        hpBar.value = currentHp;
+    }
+}
