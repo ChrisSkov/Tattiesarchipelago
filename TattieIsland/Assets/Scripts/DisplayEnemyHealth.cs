a24cd84bda11e22bf8f9fa96fb7ab0940989d649
Chris Skov <stofferdyr@gmail.com>
2020-09-26 22:35:40 +0200

    enemy hp text. 3d sounds

--- TattieIsland/Assets/Scripts/DisplayEnemyHealth.cs
+++ TattieIsland/Assets/Scripts/DisplayEnemyHealth.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class DisplayEnemyHealth : MonoBehaviour
 {
     Slider hpBar;
+    Text hpText;
+    public EnemyStats stats;
+
+
     // Start is called before the first frame update
     void Start()
     {
         hpBar = GetComponentInChildren<Slider>();
-        print(hpBar);
+        hpText = GetComponentInChildren<Text>();
     }
 
     public void SetHPBarMaxValue(float maxHP)
@@ -18,8 +22,12 @@ public class DisplayEnemyHealth : MonoBehaviour
         hpBar.maxValue = maxHP;
     }
 
+
     public void UpdateHealthBar(float currentHp)
     {
         hpBar.value = currentHp;
+        hpText.text = string.Format("{0}/{1}", Mathf.RoundToInt(currentHp), Mathf.RoundToInt(stats.maxHp));
     }
+
+
 }
