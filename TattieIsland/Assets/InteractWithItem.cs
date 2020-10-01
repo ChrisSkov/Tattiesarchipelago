781f64f5869891b9b9ed12079b774c4ad4ed708d
Chris Skov <stofferdyr@gmail.com>
2020-10-01 23:44:27 +0200

    abstract class strong together

--- TattieIsland/Assets/InteractWithItem.cs
+++ TattieIsland/Assets/InteractWithItem.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class InteractWithItem : MonoBehaviour
 {
     Animator anim;
+    public RangedWepObj obj;
+
+    public Transform hand;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,18 @@ public class InteractWithItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            obj.hand = hand;
+            GetComponent<Animator>().runtimeAnimatorController = obj.animOverride;
+            GetComponent<Animator>().SetTrigger("rightClick");
+
+        }
     }
+    public void AnimEvent()
+    {
+        var clone = Instantiate(obj.prefab, obj.hand.position, obj.hand.rotation);
+        clone.AddComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 20f, ForceMode.Impulse);
+    }
+
 }
