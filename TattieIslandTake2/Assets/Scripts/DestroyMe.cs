b0479d691cfd70601f78518743475d15070d49de
Chris Skov <stofferdyr@gmail.com>
2020-10-15 13:14:02 +0200

    organ shatter object

--- TattieIslandTake2/Assets/Scripts/DestroyMe.cs
+++ TattieIslandTake2/Assets/Scripts/DestroyMe.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class DestroyMe : MonoBehaviour
 {
     [SerializeField] float destroyTime = 2f;
-
+    [SerializeField] float destroyTimeMin = 2f;
+    [SerializeField] float destroyTimeMax = 15f;
+    [SerializeField] bool randomDestroyTime = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, destroyTime);
+        if (randomDestroyTime == true)
+        {
+
+            Destroy(gameObject, Random.Range(destroyTimeMin, destroyTimeMax));
+        }
+        else
+        {
+            Destroy(gameObject, destroyTime);
+        }
+
     }
 }
