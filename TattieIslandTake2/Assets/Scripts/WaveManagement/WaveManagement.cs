5c826bb75e98a144920f5d8f2bbe03beb670a3b7
Chris <stofferdyr@gmail.com>
2020-10-23 12:29:33 +0200

    start writing wavemanagement class

--- TattieIslandTake2/Assets/Scripts/WaveManagement/WaveManagement.cs
+++ TattieIslandTake2/Assets/Scripts/WaveManagement/WaveManagement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WaveManagement : MonoBehaviour
 {
+    public WaveSet waveSet = null;
+    public float timeBetweenWaves = 45f;
+    public float timeBetweenEnemies = 2f;
+    public bool isSpawning = false;
+    public int currentWave = 0;
+    public GameObject[] spawnPoints = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
