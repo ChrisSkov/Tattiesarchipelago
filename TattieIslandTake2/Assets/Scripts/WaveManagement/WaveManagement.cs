9306eabce1882b3bf05aa919782caea0fc657bdf
Chris Skov <stofferdyr@gmail.com>
2020-10-27 14:07:18 +0100

    minor changes to waves

--- TattieIslandTake2/Assets/Scripts/WaveManagement/WaveManagement.cs
+++ TattieIslandTake2/Assets/Scripts/WaveManagement/WaveManagement.cs
@@ -17,7 +17,7 @@ public class WaveManagement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        currentWave = 0;
     }
 
     // Update is called once per frame
