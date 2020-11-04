63f427d217dfa64e3f7423f24c497bce2e94f977
Chris Skov <stofferdyr@gmail.com>
2020-11-04 19:14:59 +0100

    import procedural UI. Flesh out level up UI

--- TattieIslandTake2/Assets/Scripts/Player/DisplayHealth.cs
+++ TattieIslandTake2/Assets/Scripts/Player/DisplayHealth.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class DisplayHealth : MonoBehaviour
 {
     public Player player;
-    Slider slider;
+    //  Slider slider;
 
     public Image image;
     Text hpText;
@@ -14,16 +14,16 @@ public class DisplayHealth : MonoBehaviour
     void Start()
     {
         hpText = GetComponentInChildren<Text>();
-        slider = GetComponentInChildren<Slider>();
-        slider.maxValue = player.stats.maxHealth;
+        // slider = GetComponentInChildren<Slider>();
+        // slider.maxValue = player.stats.maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         hpText.text = string.Format("{0}/{1}", Mathf.RoundToInt(player.stats.currentHealth), Mathf.RoundToInt(player.stats.maxHealth));
-        slider.value = player.stats.currentHealth;
+        //   slider.value = player.stats.currentHealth;
         image.fillAmount = player.stats.currentHealth / player.stats.maxHealth;
-        print(player.stats.currentHealth /player.stats.maxHealth);
+        print(player.stats.currentHealth / player.stats.maxHealth);
     }
 }
