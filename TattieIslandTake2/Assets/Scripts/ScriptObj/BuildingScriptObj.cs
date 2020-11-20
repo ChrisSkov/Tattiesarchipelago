6cef86903320b154a7b8f2276e72c5c9e40f23e9
Chris Skov <stofferdyr@gmail.com>
2020-11-20 13:42:00 +0100

    import blood asset

---
+++ TattieIslandTake2/Assets/Scripts/ScriptObj/BuildingScriptObj.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+[CreateAssetMenu]
+public class BuildingScriptObj : ScriptableObject
+{
+    public GameObject prefab;
+    public Texture2D uiImage;
+    public int price;
+    public float deliveryTime;
+    public bool canAfford;
+}
