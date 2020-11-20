fb2da305c66a618310b439eff8960325c9b1307e
Chris Skov <stofferdyr@gmail.com>
2020-11-20 22:13:15 +0100

    Start setting up store UI

---
+++ TattieIslandTake2/Assets/DisplayBuilding.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayBuilding : MonoBehaviour
+{
+    public BuildingScriptObj myBuilding;
+    Text nameText;
+    Text priceText;
+    RawImage myImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        priceText = transform.GetChild(1).GetComponent<Text>();
+        nameText = GetComponentInChildren<Text>();
+        myImage = GetComponent<RawImage>();
+        priceText.text = "X " + myBuilding.price;
+        nameText.text = myBuilding.buildingName;
+        myImage.texture = myBuilding.uiImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- TattieIslandTake2/Assets/Scripts/ScriptObj/BuildingScriptObj.cs
+++ TattieIslandTake2/Assets/Scripts/ScriptObj/BuildingScriptObj.cs
@@ -5,7 +5,9 @@ public class BuildingScriptObj : ScriptableObject
 {
     public GameObject prefab;
     public Texture2D uiImage;
+    public string buildingName;
     public int price;
+    public int woodReq;
     public float deliveryTime;
     public bool canAfford;
 }
