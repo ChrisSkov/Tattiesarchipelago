625f9be8410fbbea4d067db0784891d2721638e8
Chris Skov <stofferdyr@gmail.com>
2021-01-11 15:50:14 +0100

    main menu buttons now work as intended

---
+++ TattieIslandTake2/Assets/MainMenuButtons.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    GameObject activeMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+           CloseMenu();
+            
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenMenu(GameObject menu)
+    {
+        menu.SetActive(true);
+        activeMenu = menu;
+    }
+
+    public void CloseMenu()
+    {
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+}
