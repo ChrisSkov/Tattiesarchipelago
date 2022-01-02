29aeceedc9b81f2b9703f1dd555ae0adb9971a7f
Chris Skov <stofferdyr@gmail.com>
2022-01-02 21:56:54 +0100

    testing

--- TattieIslandTake2/Assets/Scripts/Enemies/FacePlayer.cs
+++ TattieIslandTake2/Assets/Scripts/Enemies/FacePlayer.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class FacePlayer : MonoBehaviour
 {
 
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     transform.LookAt(Camera.main.transform.position);
+    // }
+        private Transform mainCameraTransform;
     // Start is called before the first frame update
     void Start()
     {
-
+        mainCameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform.position);
+        transform.LookAt(transform.position + mainCameraTransform.rotation * Vector3.forward, mainCameraTransform.rotation * Vector3.up);
     }
 }
